46f28217f54bf03a1b066192066ef2f0436779c8
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-05 22:50:55 +0530

    Array Easy

--- ArrayEasy.js
+++ ArrayEasy.js
@@ -644,4 +644,151 @@ var maximumWealth = function(accounts) {
 };
 
 
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* 
+PROB 22 - Shuffle the Array
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+Example :
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+*/
+
+var shuffle = function(nums, n) {
+    let result = [];
+    let j = nums.length/2;
+    for(let i=0; i<nums.length/2; i++, j++){
+        result.push(nums[i], nums[j]);
+    }
+    return result;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* 
+PROB 23 - Maximum Number of Words Found in Sentences
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+Return the maximum number of words that appear in a single sentence.
+Example :
+Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+Output: 6
+Explanation: 
+- The first sentence, "alice and bob love leetcode", has 5 words in total.
+- The second sentence, "i think so too", has 4 words in total.
+- The third sentence, "this is great thanks very much", has 6 words in total.
+Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
+*/
+
+var mostWordsFound = function(sentences) {
+    let maxLength = 0;
+    for(let i=0; i<sentences.length; i++){
+        let arr = sentences[i].split(" ");
+        if(arr.length >= maxLength) maxLength = arr.length;
+    }
+    return maxLength;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* 
+PROB 24 - Number of Good Pairs
+Given an array of integers nums, return the number of good pairs.
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+Example 1:
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+*/
+
+var numIdenticalPairs = function(nums) {
+    let goodPairs = 0;
+    let map = {};
+    for(let num of nums){
+        if(map[num]){
+            goodPairs += map[num];
+            map[num]++;
+        }
+        else {
+            map[num] = 1;
+        }
+    }
+    return goodPairs;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* 
+PROB 25 - Kids With the Greatest Number of Candies
+There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and 
+an integer extraCandies, denoting the number of extra candies that you have.
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest 
+number of candies among all the kids, or false otherwise.
+Note that multiple kids can have the greatest number of candies.
+Example 1:
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+Explanation: If you give all extraCandies to:
+- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+*/
+
+var kidsWithCandies = function(candies, extraCandies) {
+    let ans = [];
+    let max = Math.max(...candies);
+    let isGreatest = false;
+    for(let i=0; i<candies.length; i++){
+        if(candies[i] + extraCandies >= max){
+            ans.push(true);
+        }
+        else {
+            ans.push(false);
+        }
+    }
+    return ans;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* 
+PROB 26 - How Many Numbers Are Smaller Than the Current Number
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count 
+the number of valid j's such that j != i and nums[j] < nums[i].
+Return the answer in an array.
+Example :
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation: 
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1). 
+For nums[3]=2 there exist one smaller number than it (1). 
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+*/
+
+var smallerNumbersThanCurrent = function(nums) {
+    let result = [];
+    let count;
+    for(let i=0; i<nums.length; i++){
+        let temp = nums[i], count = 0;
+        for(let j=0; j<nums.length; j++){
+            if(i === j) continue;
+            if(temp > nums[j]) count++;
+        }
+        result.push(count);
+    }
+    return result;
+};
+
+
+
 

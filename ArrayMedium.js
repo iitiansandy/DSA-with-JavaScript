6edcf1e9a00a7f90dde0dac91c69a1ddb240086f
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-10 01:45:52 +0530

    Array Medium

--- ArrayMedium.js
+++ ArrayMedium.js
@@ -772,6 +772,25 @@ const fourSum = function(nums, target) {
   /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 /*
-PROB 19 - 
-
+PROB 19 - Median of Two Sorted Arrays
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).
+Example :
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
 */
+
+var findMedianSortedArrays = function(nums1, nums2) {
+    let c = nums1.concat(nums2);
+    c = c.sort((a,b) => a-b);
+    if(c.length % 2 == 0){
+        let mid = c.length/2;
+        console.log(c[mid-1] + c[mid]);
+        return (c[mid-1] + c[mid])/2;
+    }
+    return c[(c.length-1)/2];
+};
+
+
+

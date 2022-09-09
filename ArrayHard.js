285313dd05fbaf1cb030545317c36d2b6b19033a
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-09 18:45:37 +0530

    Array Hard

--- ArrayHard.js
+++ ArrayHard.js
@@ -37,3 +37,28 @@ var maxSlidingWindow = function(nums, k) {
    return result
 }
 console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7],3));
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/*
+PROB 2- First Missing Positive
+Given an unsorted integer array nums, return the smallest missing positive integer.
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+Example :
+Input: nums = [1,2,0]
+Output: 3
+Explanation: The numbers in the range [1,2] are all in the array.
+*/
+
+var firstMissingPositive = function(nums) {
+    let setData = new Set(nums);
+    for(let i=1; i<=setData.size; i++){
+        if(!setData.has(i))
+            return i;
+    }
+    return setData.size + 1;
+};
+
+
+

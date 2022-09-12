7f39f3d29b7defdade381b0a614b842c22f3dda0
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-12 13:22:35 +0530

    Array Part 2

--- ArrayPart2.js
+++ ArrayPart2.js
@@ -77,7 +77,6 @@ console.log(arr.join()); // Jani,Hege,Lene,Stale,Kai Jim,Borge
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-
 /*
 PROB 3 - SEARCH ELEMENT IN AN ARRAY ()
 */
@@ -101,5 +100,36 @@ function linSearch(arr, ele){
 // Method 2 (inbuilt method)
 let arr = [4, 5, 7, 8, 9];
 let ele = 8;
-console.log(arr.indexOf(ele));
+// console.log(arr.indexOf(ele));
 
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/*
+PROB 4 - FUNCTIONAL PROGRAMMING
+Given an array of radii, calculate its area, circumference and diameter using functional programming.
+*/
+
+const area = function(radius){
+    return Math.PI * radius * radius;
+};
+
+const circumference = function(radius){
+    return 2*Math.PI * radius;
+};
+
+const diameter = function(radius){
+    return 2*radius;
+};
+
+const calculate = function(radius, logic){
+    const output = [];
+    for(let i=0; i<radius.length; i++){
+        output.push(logic(radius[i]));
+    }
+    return output;
+};
+const radius = [3, 1, 2, 4];
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumference));
+console.log(calculate(radius, diameter));

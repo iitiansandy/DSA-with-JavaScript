011a7ac5349551e30fd2c968b3a24570ffbdf5fc
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-12 16:46:49 +0530

    Leetcode Array Medium

--- ArrayMedium.js
+++ ArrayMedium.js
@@ -1189,6 +1189,105 @@ function maxSumOptimized(arr,k){
 // console.log(maxSumOptimized([3,5,1,6,7,8,9,7,6,5,4,3,4, 10, 10, 10], 3));
 
 
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
+/* PROB 29 - Find Minimum in Rotated Sorted Array
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+You must write an algorithm that runs in O(log n) time.
+Example:
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+*/
+
+var findMin = function(nums) {
+    let start = 0, end = nums.length-1;
+    let min = nums[start];
+    while(start <= end){
+        let mid = parseInt((start + end)/2);
+        if(nums[mid] < nums[start]){
+            min = Math.min(nums[mid], min);
+            end = mid - 1;
+        } else {
+            min = Math.min(nums[start], min);
+            start = mid + 1;
+        }
+    }
+    return min;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* PROB 30 - Find Peak Element
+A peak element is an element that is strictly greater than its neighbors.
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any 
+of the peaks.
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside 
+the array.
+You must write an algorithm that runs in O(log n) time.
+Example:
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+*/
+
+var findPeakElement = function(nums) {
+    let start = 0, end = nums.length-1, mid;
+    
+    while(start <= end){
+        mid = Math.floor((start + end)/2);
+        const val = nums[mid];
+        const left = nums[mid-1] || -Infinity;
+        const right = nums[mid+1] || -Infinity;
+        
+        if(val > left && val > right) return mid;
+        if(left > val){
+            end = mid-1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return 0;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* PROB 31 - Two Sum II - Input Array Is Sorted
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific 
+target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+Your solution must use only constant extra space.
+Example:
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+*/
+
+var twoSum = function(numbers, target) {
+    let end = numbers.length-1;
+    
+    for(let i=0; i<numbers.length; i++){
+        let start=i+1;
+        while (start <= end){
+            let mid = Math.floor(start + (end - start)/2);
+            if(numbers[mid] < target - numbers[i]){
+                start = mid + 1;
+            }
+            else if(numbers[mid] > target - numbers[i]){
+                end = mid - 1;
+            }
+            else if(numbers[mid] + numbers[i] === target){
+                return [i+1, mid+1];
+            }
+        }
+    }
+};
 
 

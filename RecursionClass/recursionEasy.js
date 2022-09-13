48f87ed84ac36b44783aefe21465cbe2471d681e
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-13 20:23:38 +0530

    Recursion Problems

--- RecursionClass/recursionEasy.js
+++ RecursionClass/recursionEasy.js
@@ -78,3 +78,94 @@ function run(n, i=0){
 }
 
 
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/*
+PROB 4 - Tower of Hanoi
+Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move all disks from source rod to 
+destination rod using third rod (say auxiliary). The rules are :
+1) Only one disk can be moved at a time.
+2) A disk can be moved only if it is on the top of a rod.
+3) No disk can be placed on the top of a smaller disk.
+Print the steps required to move n disks from source rod to destination rod.
+Source Rod is named as 'a', auxiliary rod as 'b' and destination rod as 'c'.
+Sample Input:
+3
+Sample Output:
+a c
+a b
+c b
+a c
+b a
+b c
+a c
+*/
+
+function towerOfHanoi(disks, source, auxiliary, destination){
+    if(disks === 0) return;
+
+    else {
+        towerOfHanoi(disks-1 , source, destination, auxiliary);
+        console.log(source + " " + destination);
+        towerOfHanoi(disks-1, auxiliary, source, destination);
+    }
+}
+// towerOfHanoi(3, 'a', 'b', 'c');
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/*
+PROB 5 - Replace pi (recursive)
+Given a string, compute recursively a new string where all appearances of "pi" have been replaced by "3.14".
+Sample Input 1 : xpix
+Sample Output : x3.14x
+*/
+
+function replacePI(str){
+    let len = str.length;
+    let ans = "";
+ 
+    if(len <=1) return str;
+
+    if(str.charAt(0) === 'p' && str.charAt(1) === 'i'){
+        ans = "3.14" + replacePI(str.substring(2,len));
+        return ans;
+    }
+
+    ans = str.charAt(0) + replacePI(str.substring(1,len));
+    return ans;
+}
+// console.log(replacePI('pipi'));
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/*
+PROB 6 - Remove X
+Given a string, compute recursively a new string where all 'x' chars have been removed.
+Sample Input 1 : xaxb
+Sample Output 1: ab
+*/
+
+function removeX(str){
+    let len = str.length;
+    let ans = "";
+    if(len === 0) return str;
+
+    if(str.charAt(0) === 'x'){
+        return removeX(str.substring(1,len));
+    }
+    ans = str.charAt(0) + removeX(str.substring(1,len));
+    return ans;
+}
+// console.log(removeX('xaxb'));
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/*
+PROB 7 - 
+
+*/
+

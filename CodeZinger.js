ffeaa5701ebe2f58342b6368aecc481605092d3f
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-15 23:43:28 +0530

    Codezinger Problems

--- CodeZinger.js
+++ CodeZinger.js
@@ -76,16 +76,42 @@ console.log(counter("hello")); // {h: 1, e: 1, l: 2, o: 1}
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-/* 
-PROB - Frequency of characters in a string
+/*
+Prob - Check whether a given string is an interleaving of two other given strings
+Given three strings S1, S2 and S3. Write a program that checks whether S3 is an interleaving of S1 and S2. 
+S3 is said to be an interleaving of S1 and S2 if it contains all characters of S1 and S2 and order of all characters in individual 
+strings is preserved.
+Input:
+    AB
+    CD
+    ACBD
+    where:
+First line represents input string S1
+Second line represents input string S2
+Third line represents input string S3
+
+Output: Yes
 */
 
+function interLeaving(str1, str2, str3){
+  let i=0, j=0, k=0;
+  while (k != str3.length){
+    if(i < str1.length && str1[i] == str3[k]){
+      i++;
+    } else if(j < str2.length && str2[j] == str3[k]){
+      j++;
+    } else {
+      return "No";
+    }
+    k++;
+  }
+  if(i < str1.length || j < str2.length){
+    return "No";
+  }
+  return "Yes";
+}
 
-
-
-/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-
-/* 
-PROB 3 - Frequency of characters in a string
-*/
-
+let str1 = "AB";
+let str2 = "CD";
+let str3 = "ABCD";
+console.log(interLeaving(str1, str2, str3));

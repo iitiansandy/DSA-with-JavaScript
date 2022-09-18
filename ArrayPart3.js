b556a692446c56b3ad4270db305d80c37fba45a0
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-18 12:51:41 +0530

    Leetcode Array Easy

--- ArrayPart3.js
+++ ArrayPart3.js
@@ -203,6 +203,181 @@ var largestAltitude = function(gain) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 /*
-Prob. 
+Prob: Number Of Rectangles That Can Form The Largest Square (Leetcode 1725)
+You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
+You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example, if you have a rectangle [4,6], 
+you can cut it to get a square with a side length of at most 4.
+Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
+Return the number of rectangles that can make a square with a side length of maxLen.
+Example :
+Input: rectangles = [[5,8],[3,9],[5,12],[16,5]]
+Output: 3
+Explanation: The largest squares you can get from each rectangle are of lengths [5,3,5,5].
+The largest possible square is of length 5, and you can get it out of 3 rectangles.
+ */
 
+// Method 1
+var countGoodRectangles = function (rectangles) {
+    let map = new Map()
+
+    rectangles.forEach(x => {
+        if (x[0] < x[1]) map.set(x[0], map.get(x[0]) + 1 || 1)
+        else map.set(x[1], map.get(x[1]) + 1 || 1)
+
+    })
+
+    return map.get(Math.max(...map.keys()))
+};
+
+
+// Method 2
+var countGoodRectangles = function (rectangles) {
+    let count = 0;
+    let max = 0;
+
+    for (let i of rectangles) {
+
+        let side = Math.min(i[0], i[1]);
+
+        if (side > max) {
+            max = side;
+            count = 1;
+        } else if (side == max) {
+            count++;
+        }
+    }
+    return count;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/*
+ Prob - Find First Palindromic String in the Array
+ Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+A string is palindromic if it reads the same forward and backward.
+Example:
+
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada".
+Note that "racecar" is also palindromic, but it is not the first.
 */
+
+// VERSION 1 : TWO POINTER
+var firstPalindrome = function (words) {
+    for (w of words) {
+        start = 0
+        end = w.length - 1;
+        while (start < end && w[start] == w[end]) {
+            start++;
+            end--;
+        }
+        if (w[start] == w[end]) {
+            return w;
+        }
+    }
+    return ""
+};
+
+// VERSION 2: USING BUILT-IN METHODS
+
+var firstPalindrome = function (words) {
+    for (w of words) {
+        if (w == w.split('').reverse().join(''))
+            return w
+    }
+    return ""
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* Prob - Cells with Odd Values in a Matrix
+There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed 
+location to perform some increment operations on the matrix.
+For each location indices[i], do both of the following:
+Increment all the cells on row ri.
+Increment all the cells on column ci.
+Given m, n, and indices, return the number of odd-valued cells in the matrix after applying the increment to all locations in indices.
+Input: m = 2, n = 3, indices = [[0,1],[1,1]]
+Output: 6
+Explanation: Initial matrix = [[0,0,0],[0,0,0]].
+After applying first increment it becomes [[1,2,1],[0,1,0]].
+The final matrix is [[1,3,1],[1,3,1]], which contains 6 odd numbers.
+*/
+
+var oddCells = function(m, n, indices) {
+    const matrix = Array.from(Array(m), () => Array(n).fill(0));
+    
+    let res = 0;
+    for (const [r, c] of indices) {
+        for (let i = 0; i < n; i++) {
+            matrix[r][i] ^= 1;
+            if (matrix[r][i]) res++; else res--;
+        }
+        for (let i = 0; i < m; i++) {
+            matrix[i][c] ^= 1;
+            if (matrix[i][c]) res++; else res--;
+        }
+    }
+    return res;
+};
+
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* Prob - Find N Unique Integers Sum up to Zero
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+Example:
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+*/
+
+var sumZero = function(n) {
+    let res = new Array(n);
+    for(let i=0; i<n; i++){
+        res[i] = i*2 - n + 1;
+    }
+    return res;
+};
+
+
+// Method 2
+var sumZero = function(n) {
+    let arr = [];
+    for(let i = 0 - Math.floor(n / 2) ; arr.length < n; i++){
+       if(i === 0 && n %2 === 0) continue;
+        arr.push(i)
+    }
+    return arr;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* Prob - Find Target Indices After Sorting Array
+You are given a 0-indexed integer array nums and a target element target.
+A target index is an index i such that nums[i] == target.
+Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list.
+The returned list must be sorted in increasing order.
+Example:
+Input: nums = [1,2,5,2,3], target = 2
+Output: [1,2]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The indices where nums[i] == 2 are 1 and 2.
+*/
+
+var targetIndices = function(nums, target) {
+    nums = nums.sort((a, b) => a - b);
+    let ans = []
+    for(let i = 0; i < nums.length; i++)
+        if(nums[i] == target)
+            ans.push(i)
+    return ans;
+};
+
+
+

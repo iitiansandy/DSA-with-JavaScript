4bd644d23d8c1ad77823861299572e1b016fe489
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-25 20:32:35 +0530

    Leetcode Array Medium

--- ArrayMedium2.js
+++ ArrayMedium2.js
@@ -166,4 +166,99 @@ var searchMatrix = function(matrix, target) {
 };
 
 
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+PROB 3: Minimum Amount of Time to Collect Garbage
+You are given a 0-indexed array of strings garbage where garbage[i] represents the assortment of garbage at the ith house. garbage[i] consists only of 
+the characters 'M', 'P' and 'G' representing one unit of metal, paper and glass garbage respectively. Picking up one unit of any type of garbage takes 1 
+minute.
+You are also given a 0-indexed integer array travel where travel[i] is the number of minutes needed to go from house i to house i + 1.
+There are three garbage trucks in the city, each responsible for picking up one type of garbage. Each garbage truck starts at house 0 and must visit 
+each house in order; however, they do not need to visit every house.
+Only one garbage truck may be used at any given moment. While one truck is driving or picking up garbage, the other two trucks cannot do anything.
+Return the minimum number of minutes needed to pick up all the garbage.
+
+Example 1:
+
+Input: garbage = ["G","P","GP","GG"], travel = [2,4,3]
+Output: 21
+Explanation:
+The paper garbage truck:
+1. Travels from house 0 to house 1
+2. Collects the paper garbage at house 1
+3. Travels from house 1 to house 2
+4. Collects the paper garbage at house 2
+Altogether, it takes 8 minutes to pick up all the paper garbage.
+The glass garbage truck:
+1. Collects the glass garbage at house 0
+2. Travels from house 0 to house 1
+3. Travels from house 1 to house 2
+4. Collects the glass garbage at house 2
+5. Travels from house 2 to house 3
+6. Collects the glass garbage at house 3
+Altogether, it takes 13 minutes to pick up all the glass garbage.
+Since there is no metal garbage, we do not need to consider the metal garbage truck.
+Therefore, it takes a total of 8 + 13 = 21 minutes to collect all the garbage.
+*/
+
+const garbageCollection = (garbage, travel) => {
+    let travelTime = 0
+    garbage = garbage.reverse()
+    
+    for (const type of ['G', 'P', 'M']) {
+      const lastHouseWithGarbage = garbage.findIndex(house => house.includes(type))
+      
+      if (lastHouseWithGarbage === -1) {
+        continue
+      }
+  
+      travelTime += travel.slice(0, garbage.length - lastHouseWithGarbage - 1).reduce((acc, num) => acc + num, 0)
+    }
+  
+    return garbage.join('').length + travelTime
+  };
+
+
+  /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob - Partition Array According to Given Pivot
+You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+
+Every element less than pivot appears before every element greater than pivot.
+Every element equal to pivot appears in between the elements less than and greater than pivot.
+The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+More formally, consider every pi, pj where pi is the new position of the ith element and pj is the new position of the jth element. For elements less than pivot, if i < j and nums[i] < pivot and nums[j] < pivot, then pi < pj. Similarly for elements greater than pivot, if i < j and nums[i] > pivot and nums[j] > pivot, then pi < pj.
+Return nums after the rearrangement.
+
+Example:
+Input: nums = [9,12,5,10,14,3,10], pivot = 10
+Output: [9,5,3,10,10,12,14]
+Explanation: 
+The elements 9, 5, and 3 are less than the pivot so they are on the left side of the array.
+The elements 12 and 14 are greater than the pivot so they are on the right side of the array.
+The relative ordering of the elements less than and greater than pivot is also maintained. [9, 5, 3] and [12, 14] are the respective orderings.
+*/
+
+var pivotArray = function(nums, pivot) {
+    let left = [];
+    let right = [];
+    let center = [];
+    
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] < pivot){
+            left.push(nums[i]);
+        } else if(nums[i] > pivot){
+            right.push(nums[i]);
+        } else {
+            center.push(nums[i]);
+        }
+    }
+    return [...left, ...center, ...right];
+};
+
+
 

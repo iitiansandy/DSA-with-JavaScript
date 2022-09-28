a221531b44b93ed0bb8cc25a94e62ad1c82db48c
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-09-28 13:00:00 +0530

    Leetcode Array Medium

--- ArrayMedium2.js
+++ ArrayMedium2.js
@@ -532,5 +532,77 @@ var wateringPlants = function(plants, capacity) {
 };
 
 
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
+
+/*
+ Prob - Arithmetic Subarrays
+ A sequence of numbers is called arithmetic if it consists of at least two elements, and the difference between every two consecutive elements is the same. 
+ More formally, a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] for all valid i.
+
+For example, these are arithmetic sequences:
+
+1, 3, 5, 7, 9
+7, 7, 7, 7
+3, -1, -5, -9
+The following sequence is not arithmetic:
+
+1, 1, 2, 5, 7
+You are given an array of n integers, nums, and two arrays of m integers each, l and r, representing the m range queries, where the ith query is the 
+range [l[i], r[i]]. All the arrays are 0-indexed.
+
+Return a list of boolean elements answer, where answer[i] is true if the subarray nums[l[i]], nums[l[i]+1], ... , nums[r[i]] can be rearranged to form 
+an arithmetic sequence, and false otherwise.
+
+Example:
+
+Input: nums = [4,6,5,9,3,7], l = [0,0,2], r = [2,3,5]
+Output: [true,false,true]
+Explanation:
+In the 0th query, the subarray is [4,6,5]. This can be rearranged as [6,5,4], which is an arithmetic sequence.
+In the 1st query, the subarray is [4,6,5,9]. This cannot be rearranged as an arithmetic sequence.
+In the 2nd query, the subarray is [5,9,3,7]. This can be rearranged as [3,5,7,9], which is an arithmetic sequence.
+ */
+
+var checkArithmeticSubarrays = function(nums, l, r) {
+    let res = [];
+    for(let i=0; i<l.length && i<r.length; i++){
+        let left = l[i];
+        let right = r[i];
+        let range = nums.slice(left, right+1);
+        
+        res.push(isSequence(range));
+    }
+    return res;
+};
+
+var isSequence = function(nums){
+    if(nums.length < 2){
+        return false;
+    }
+    nums = nums.sort((a,b)=> a-b);
+    let dist;
+    
+    for(let i=0; i<nums.length-1; i++){
+        let prev = nums[i];
+        let next = nums[i+1];
+        let delta = next - prev;
+        
+        if(dist === undefined){
+            dist = delta;
+        } else if(dist !== delta){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob - 
+
+*/
 

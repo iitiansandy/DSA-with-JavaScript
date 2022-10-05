d65f2b9ddae77e45e75f8741f7fc714fe8602ae4
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-05 13:19:16 +0530

    Interview Questions

--- InterviewQuestions.js
+++ InterviewQuestions.js
@@ -952,6 +952,119 @@ function getFrequency(string) {
 
     return freq;
 };
-console.log(getFrequency("geeksforgeeks"));
+// console.log(getFrequency("geeksforgeeks"));
 
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Length of the longest substring without repeating characters
+Given a string str, find the length of the longest substring without repeating characters.
+Example: Input: s = "abcabcbb"
+Output: 3
+*/
+
+function longestSubStr(str){
+    if(!str){
+        return 0;
+    }
+    let start = 0;
+    let end = 0;
+    let maxlen = 0;
+    let uniqueChars = new Set();
+
+    while(end < str.length){
+        if(!uniqueChars.has(str[end])){
+            uniqueChars.add(str[end]);
+            end++;
+            maxlen = Math.max(maxlen, uniqueChars.size);
+        } else {
+            uniqueChars.delete(str[start]);
+            start++;
+        }
+    }
+    return maxlen;
+};
+// let str = "abcabcbb";
+// console.log(longestSubStr(str));
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Largest Sum Contiguous Subarray (Kadane’s Algorithm)
+Given an array arr[] of size N. The task is to find the sum of the contiguous subarray within a arr[] with the largest sum. 
+Example: Input: [-2, -3, 4, -1, -2, 1, 5, -3]
+Output: Maximum contiguous sum is 7
+*/
+
+function maxSubArr(arr){
+    var maxInt = Math.pow(2, 53);
+    var maxSoFar = -maxInt - 1;
+    var maxEndingHere = 0;
+
+    for(var i=0; i<arr.length; i++){
+        maxEndingHere = maxEndingHere + arr[i];
+        if(maxSoFar < maxEndingHere){
+            maxSoFar = maxEndingHere;
+        };
+        if(maxEndingHere < 0){
+            maxEndingHere = 0;
+        }
+    }
+    return maxSoFar;
+}
+// var arr = [ -2, -3, 4, -1, -2, 1, 5, -3 ];
+// console.log(maxSubArr(arr));
+
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Sliding Window Maximum
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. 
+You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+Return the max sliding window.
+
+Example 1:
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+*/
+
+var maxSlidingWindow = function(nums, k) {
+    let res = [];
+    let q = [];
+
+    for(let i=0; i<nums.length; i++){
+        while(q.length-1 >= 0 && nums[i] > q[q.length-1]){
+            q.pop();
+        }
+        q.push(nums[i]);
+        // When i + 1 - k >= 0, the window is fully overlapping nums
+        const j = i + 1 - k;
+        if(j >= 0){
+            res.push(q[0]);
+            if(nums[j] === q[0]){
+                q.shift(); // If the biggest element in q is about to exit window, remove it from q
+            }
+        }
+    }
+    return res;
+};
+// let nums = [1, 2, 3, 1, 4, 5, 2, 3, 6]
+// let k = 3;
+// console.log(maxSlidingWindow(nums,k));
 

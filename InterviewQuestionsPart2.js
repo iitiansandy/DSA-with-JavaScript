76c6e92f7aaaa06777e66c112dca9fa8f19f192c
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-10 22:46:43 +0530

    Interview Questions

--- InterviewQuestionsPart2.js
+++ InterviewQuestionsPart2.js
@@ -373,3 +373,260 @@ var lowestCommonAncestor = function(root, p, q) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Find the Winner of the Circular Game
+There are n friends that are playing a game. The friends are sitting in a circle and are numbered from 1 to n in clockwise order. More formally, moving 
+clockwise from the ith friend brings you to the (i+1)th friend for 1 <= i < n, and moving clockwise from the nth friend brings you to the 1st friend.
+The rules of the game are as follows:
+
+Start at the 1st friend.
+Count the next k friends in the clockwise direction including the friend you started at. The counting wraps around the circle and may count some friends more than once.
+The last friend you counted leaves the circle and loses the game.
+If there is still more than one friend in the circle, go back to step 2 starting from the friend immediately clockwise of the friend who just lost and repeat.
+Else, the last friend in the circle wins the game.
+Given the number of friends, n, and an integer k, return the winner of the game.
+
+Example : Input: n = 5, k = 2
+Output: 3
+*/
+
+var findTheWinner = function(n, k) {
+    let y = n;
+    function ans(n,k){
+        if(n === 1){
+            return 0;
+        } else {
+            return (ans(n-1,k)+k)%(n);
+        }
+    }
+    return ans(n,k)+1;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Count Good Numbers
+A digit string is good if the digits (0-indexed) at even indices are even and the digits at odd indices are prime (2, 3, 5, or 7).
+Given an integer n, return the total number of good digit strings of length n. Since the answer may be large, return it modulo 109 + 7.
+
+A digit string is a string consisting of digits 0 through 9 that may contain leading zeros.
+
+Example 1:
+
+Input: n = 1
+Output: 5
+Explanation: The good numbers of length 1 are "0", "2", "4", "6", "8".
+*/
+
+var countGoodNumbers = function(n) {
+    let mod = 1000000007n;
+    let even = Math.ceil(n/2);
+    let odd = n - even;
+
+    function myPow(x,y){
+        if(y === 0){
+            return 1n;
+        }
+        let res = 1n;
+        res *= myPow(x, Math.floor(y/2));
+        res *= res;
+
+        if(y%2 === 1){
+            res *= BigInt(x);
+        }
+        res %= mod;
+        return res;
+    }
+    let ans = myPow(5,even) * myPow(4, odd);
+    ans %= mod;
+    return Number(ans);
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Minimum Non-Zero Product of the Array Elements
+You are given a positive integer p. Consider an array nums (1-indexed) that consists of the integers in the inclusive range [1, 2p - 1] in their binary 
+representations. You are allowed to do the following operation any number of times:
+
+Choose two elements x and y from nums.
+Choose a bit in x and swap it with its corresponding bit in y. Corresponding bit refers to the bit that is in the same position in the other integer.
+For example, if x = 1101 and y = 0011, after swapping the 2nd bit from the right, we have x = 1111 and y = 0001.
+
+Find the minimum non-zero product of nums after performing the above operation any number of times. Return this product modulo 109 + 7.
+
+Note: The answer should be the minimum product before the modulo operation is done.
+
+Example:
+
+Input: p = 1
+Output: 1
+Explanation: nums = [1].
+There is only one element, so the product equals that element.
+*/
+
+var minNonZeroProduct = function(p) {
+    p = BigInt(p);
+    const mod = BigInt(1e9 + 7);
+    const first = ((1n << p) - 1n);
+    const half = first/2n;
+    const second = powMod(first - 1n, half);
+    return (first * second) % mod;
+
+    function powMod(num, exp){
+        if(exp === 0n) return 1n;
+        const temp = powMod(num, exp >> 1n);
+        let res = (temp * temp) % mod;
+        if(exp % 2n){
+            res = (res * num) % mod;
+        }
+        return res;
+    }
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Regular Expression Matching
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Example :
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+*/
+
+var isMatch = function(s, p) {
+    if(!p){
+        return !s;
+    }
+    const firstMatchChar = Boolean(s) && (p[0] === '.' || p[0] === s[0]);
+    if(p[1] === '*'){
+        return (
+            isMatch(s,p.slice(2)) || 
+            (firstMatchChar && isMatch(s.slice(1),p))
+        );
+    }
+    return firstMatchChar ? isMatch(s.slice(1), p.slice(1)) : false;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Capitalize the first letter of all the elements of an array of strings.
+Example: Input: arr = [hi, hello, akash];
+Output: [Hi, Hello, Akash];
+*/
+
+function capitalizeFirstLetter(arr){
+    if(arr.length === 0){
+        return [];
+    }
+    let res = [];
+    let s = arr[0][0].toUpperCase() + arr[0].slice(1);
+    res.push(s);
+    return res.concat(capitalizeFirstLetter(arr.slice(1)));
+}
+// let arr = ["hi", "hello", "akash"];
+// console.log(capitalizeFirstLetter(arr));
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Reverse a string using recursion like following:
+Input: "hi to all"
+Output: "ih ot lla"
+*/
+
+function reverse(str){
+    if(str.length === 0){
+        return "";
+    }
+    return reverse(str.slice(1)) + str[0];
+}
+
+function revSentenceWords(sentence){
+    let words = sentence.split(" ");
+    for(let i=0; i<words.length; i++){
+        words[i] = reverse(words[i]);
+    }
+    return words.join(" ");
+}
+
+// let str = "hi to all";
+// console.log(revSentenceWords(str));
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Print the duplicates in an array
+Example: Input: arr = [2,5,6,5,7,2];
+Output: 2, 5
+*/
+
+function duplicates(arr){
+    let map = new Map();
+    for(let i=0; i<arr.length; i++){
+        if(map.get(arr[i]) === undefined){
+            map.set(arr[i], 1);
+        } else {
+            map.set(arr[i], map.get(arr[i]) + 1);
+        }
+    }
+    let res = [];
+    for(let pairs of map){// map contains pairs of [key, value] ==> [key[0], value[1]]
+        if(pairs[1] > 1){ // if value[1] > 1, collect the key of that pair
+            res.push(pairs[0]);
+        }
+    }
+    return res;
+}
+// let arr = [2,5,6,5,7,2];
+// console.log(duplicates(arr));
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Check whether an array is pelindromic or not
+Example: Input: arr = [3,6,0,6,3]
+Output: Yes
+*/
+
+function checkPelindrome(arr){
+    let flag = 0;
+    let n = arr.length;
+    for(let i=0; i<n/2 && n != 0; i++){
+        if(arr[i] !== arr[n-i-1]){
+            flag = 1;
+            break;
+        }
+    }
+    if(flag === 1){
+        return "No";
+    } else {
+        return "Yes";
+    }
+}
+// let arr = [3,6,0,6,3,0];
+// console.log(checkPelindrome(arr));
+
+

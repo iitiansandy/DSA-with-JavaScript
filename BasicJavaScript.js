c5f2b1bcb443c1a9d5c9abf4759c6e1bbe993c7a
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-14 23:40:16 +0530

    Basic JavaScript

---
+++ BasicJavaScript.js
@@ -0,0 +1,34 @@
+/**** Section 1👉 Data Types in JavaScript ****/
+
+
+// var myName = "sandeep kumar";
+// console.log(myName);
+
+// var myAge = 26;
+// console.log(myAge);
+
+// var iAmThapas = false;
+// console.log(iAmSandeep);
+
+// // typeof operator 
+// console.log(typeof(iAmSandeep));
+
+
+// DataTypes Practice
+
+// console.log( 10 + "20");
+// 9 - "5"
+// console.log( 9 - "5"); //bug
+// "Java" + "Script"
+// console.log( "Java "+ "Script");
+// " " + " "
+// console.log( " " + 0);
+// " " + 0
+// "sandeep" - "kumar"
+// true + true
+// true + false
+// false + true
+// false - true
+
+
+// console.log("sandeep" - "kumar");

ff6c3a619e36a4973b65d0d1309dbd5bf73c859e
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-15 22:42:33 +0530

    Binary Tree

--- BinaryTree.js
+++ BinaryTree.js
@@ -27,6 +27,52 @@ var isBalanced = function(root) {
 
 
 /*
-Prob: 
+Prob: Diameter of Binary Tree
+Given the root of a binary tree, return the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+The length of a path between two nodes is represented by the number of edges between them.
 
+Example: Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
 */
+
+// Method 1
+var diameterOfBinaryTree = function(root) {
+    let diameter = 0;
+    dfs(root);
+    return diameter;
+
+    function dfs(node, level){
+        if(!node) return 0;
+
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+
+        // update diameter at every node
+        diameter = Math.max(diameter, left+right);
+
+        // update the largest number of edge so far
+        return 1+Math.max(left, right);
+    }
+};
+
+
+// Method 2
+var diameterOfBinaryTree = function(root) {
+    let diameter = 0;
+
+    function helper(root){
+        if(!root) return -1;
+
+        const lh = 1 + helper(root.left);
+        const rh = 1 + helper(root.right);
+        diameter = Math.max(diameter, lh + rh);
+        return Math.max(lh, rh);
+    }
+    helper(root);
+    return diameter;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */

00e36a60dc86b4a2f1601ffab2b59f23d7085cd7
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-17 12:29:11 +0530

    Interview Questions

--- InterviewQuestions.js
+++ InterviewQuestions.js
@@ -844,17 +844,20 @@ function kthSmallEle(nums,k){
 //Time complexity: O((mx-mn) * log (mx-mn));
 
 
-/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
 /*
 Prob: Sort an array of 0s, 1s and 2s | Dutch National Flag problem
-Given an array A[] consisting of only 0s, 1s, and 2s. The task is to write a function that sorts the given array. The functions should put all 0s first, then all 1s and all 2s in last.
+Given an array A[] consisting of only 0s, 1s, and 2s. The task is to write a function that sorts the given array. The functions should put all 0s first, 
+then all 1s and all 2s in last.
 
 This problem is also the same as the famous “Dutch National Flag problem”. The problem was proposed by Edsger Dijkstra. The problem is as follows:
 
-Given N balls of colour red, white or blue arranged in a line in random order. You have to arrange all the balls such that the balls with the same colours are adjacent with the order of 
-the balls, with the order of the colours being red, white and blue (i.e., all red coloured balls come first then the white coloured balls and then the blue coloured balls). 
+Given N balls of colour red, white or blue arranged in a line in random order. You have to arrange all the balls such that the balls with the same colours 
+are adjacent with the order of 
+the balls, with the order of the colours being red, white and blue (i.e., all red coloured balls come first then the white coloured balls and then the 
+    blue coloured balls). 
 Example:  
 Input: {0, 1, 2, 0, 1, 2}
 Output: 0, 0, 1, 1, 2, 2}

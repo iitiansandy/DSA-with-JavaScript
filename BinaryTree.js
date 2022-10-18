466d8a5fc993bece1399d9def83d824db8d53de8
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-19 00:00:08 +0530

    Binary Tree

--- BinaryTree.js
+++ BinaryTree.js
@@ -116,3 +116,32 @@ var maxDepth = function(root) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Binary Tree Level Order Traversal
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+Example:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+*/
+
+var levelOrder = function(root) {
+    let q = [root], ans = [];
+
+    while(q[0]){
+        let qlen = q.length, row = [];
+        for(let i=0; i<qlen; i++){
+            let curr = q.shift();
+            row.push(curr.val);
+            if(curr.left) q.push(curr.left);
+            if(curr.right) q.push(curr.right);
+        }
+        ans.push(row);
+    }
+    return ans;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+

e306b8181b1ecf1f3285875131986554af143cf5
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-19 20:09:37 +0530

    Binary Tree

--- BinaryTree.js
+++ BinaryTree.js
@@ -145,3 +145,32 @@ var levelOrder = function(root) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Validate Binary Search Tree
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+Example:
+Input: root = [2,1,3]
+Output: true
+*/
+
+var isValidBST = function(root, min=null, max=null) {
+    if(!root) return true;
+    if(min && root.val <= min.val) return false;
+    if(max && root.val >= max.val) return false;
+    return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: 
+
+*/

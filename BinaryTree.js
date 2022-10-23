6815d7ec6b502993f7f74aebc1c1ccc9c0466a54
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-23 22:36:40 +0530

    Binary Tree

--- BinaryTree.js
+++ BinaryTree.js
@@ -170,7 +170,48 @@ var isValidBST = function(root, min=null, max=null) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
-/*
-Prob: 
+/* 
+Prob: Lowest Common Ancestor of a Binary Tree
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that 
+has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Example: Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
 */
+
+// Method 1
+var lowestCommonAncestor = function(root, p, q) {
+    if(!root || root === p || root === q) return root;
+    var resL = lowestCommonAncestor(root.left, p, q);
+    var resR = lowestCommonAncestor(root.right, p, q);
+    return (resL && resR) ? root : (resL || resR);
+};
+
+
+// Method 2
+var lowestCommonAncestor = function(root, p, q) {
+    const dfs = (node) => {
+      if (node === null) {
+        return null;
+      }
+    
+      if (node === p || node === q) {
+        return node 
+        
+      }
+      
+      const left = dfs(node.left);
+      const right = dfs(node.right);
+      
+      return left && right ? node : left || right;
+    }
+    
+    return dfs(root);
+  };
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+

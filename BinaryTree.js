2bc06cde2a53863cb238b4f9c5945dba0271a85c
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-25 23:39:02 +0530

    Binary Tree

--- BinaryTree.js
+++ BinaryTree.js
@@ -215,3 +215,101 @@ var lowestCommonAncestor = function(root, p, q) {
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
+
+/* 
+Prob: Number of Islands
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+Example:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+*/
+const numIslands =  (grid) => {
+	let count = 0 // the counted islands
+	//Go though each cell of the 2d array/grid 
+	for(let row = 0; row < grid.length; row++){
+	for(let col = 0; col < grid[row].length; col ++){
+	if(grid[row][col] == '1'){
+		count ++
+		explore(row,col, grid)
+            }
+        }
+    }
+    return count
+}
+
+
+
+// Takes a cell in a grid with a “1” , turns it into a “0” and explores (DFS) any of the left, right, up, down 1’s
+function explore(row, col, grid){
+    //Let's return IF
+    // row < 0 OR col < 0 OR row is out of bounds(meaning the row is larger than the number of arrays in the 2d array) OR col is at/out of bounds (meaning the current col is at/over the number of elements a row has.)
+     if (row < 0 || col < 0 || row >= grid.length  
+         || col >= grid[row].length || grid[row][col] === '0')  {
+        return
+    }
+    
+    //Otherwise, we should explore it!
+    //First let's set the current spot to "0"
+    grid[row][col]='0'
+    
+	//Possibilites:
+	// 1) 1 to the right, left, top, bottom
+	//right
+	explore(row, col+1, grid)   
+    //Left
+	explore(row, col-1, grid)  
+    //Down
+	explore(row+1, col, grid) 
+    //Up
+	explore(row-1, col, grid)   
+
+}
+let grid = [
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+  ];
+  console.log(numIslands(grid));
+
+
+  // Method 2
+  var numIslands1 = function(grid) {
+    
+    const callDFS = (i, j) => {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0') return;
+        
+        grid[i][j] = '0';
+        
+        callDFS(i-1, j); // up
+        callDFS(i+1, j); // down
+        callDFS(i, j-1); // left
+        callDFS(i, j+1); // right
+    }
+    
+    let count = 0;
+    for(let i = 0; i < grid.length; i++) {
+        
+        for(let j = 0; j < grid[i].length; j++) {
+            
+            if(grid[i][j] === '1') {
+                count++;
+                callDFS(i, j)
+            }
+        }
+    }
+    return count;
+};
+
+
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */

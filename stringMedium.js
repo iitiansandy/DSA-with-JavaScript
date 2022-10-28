111852ae75e66f50fba139ab869de1df2d5e8458
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-10-28 23:22:22 +0530

    String Leetcode Medium

--- stringMedium.js
+++ stringMedium.js
@@ -280,6 +280,7 @@ var countAndSay = function(n) {
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
+
 /*
 PROB 9 - Reverse Words in a String
 Given an input string s, reverse the order of the words.
@@ -308,6 +309,58 @@ function revWords(str){
     return reverseWords;
 }
 
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+/*
+Prob: Find All Anagrams in a String
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example:
+
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+*/
+
+var findAnagrams = function(s, p) {
+    const output = [];
+    const neededChars = {};
+
+    for(let char of p){
+        if(char in neededChars){
+            neededChars[char]++;
+        } else {
+            neededChars[char] = 1;
+        }
+    }
+
+    let left=0, right=0, count = p.length;
+
+    while(right < s.length){
+        if(neededChars[s[right]] > 0) count--;
+
+        neededChars[s[right]]--;
+        right++;
+
+        if(count === 0) output.push(left);
+
+        if(right - left == p.length){
+            if(neededChars[s[left]] >= 0) count++;
+            neededChars[s[left]]++;
+            left++;
+        }
+    }
+    return output;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
 

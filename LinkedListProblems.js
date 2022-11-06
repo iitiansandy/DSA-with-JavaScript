4955c4eaca418eb2db40be0175ac3b691dccaf29
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-06 23:18:54 +0530

    Linked List

--- LinkedListProblems.js
+++ LinkedListProblems.js
@@ -107,7 +107,158 @@ var middleNode = function(head) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
-/* 
+/*
+Prob: Remove Nth Node From End of List
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example: Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+*/
+
+// Method 1
+var removeNthFromEnd = function(head, n) {
+    let fast = head, slow = head;
+    for(let i=0; i<n; i++){
+        fast = fast.next;
+    }
+    if(!fast){
+        return head.next;
+    }
+    while(fast.next){
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return head;
+};
+
+
+// Method 2
+var removeNthFromEnd = function(head, n) {
+    let fast = head, slow = head;
+    while(n--){
+        fast = fast.next;
+    }
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    if(!fast){
+        head = head.next;
+    } else {
+        slow.next = slow.next ? slow.next.next : null;
+    }
+    return head;
+};
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Merge k Sorted Lists
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+*/
+
+// Method 1
+function mergeLists(a, b){
+    const dummy = new ListNode(0);
+    let temp = dummy;
+    while(a !== null && b !== null){
+        if(a.val < b.val){
+            temp.next = a;
+            a = a.next;
+        } else {
+            temp.next = b;
+            b = b.next;
+        }
+        temp = temp.next;
+    }
+    if(a !== null){
+        temp.next = a;
+    }
+    if(b !== null){
+        temp.next = b;
+    }
+    return dummy.next;
+}
+
+var mergeKLists = function(lists) {
+    if(lists.length === 0){
+        return null;
+    }
+    while(lists.length > 1) {
+        let a = lists.shift();
+        let b = lists.shift();
+        const h = mergeLists(a, b);
+        lists.push(h);
+    }
+    return lists[0];
+};
+
+
+// Method 2
+function merge (left, right){
+    if(!left){
+        return right;
+    } else if(!right) {
+        return left;
+    } else if(left.val < right.val) {
+        left.next = merge(left.next, right);
+        return left;
+    } else {
+        right.next = merge(left, right.next);
+        return right;
+    }
+}
+
+function helper (lists, start, end) {
+    if(start === end) {
+        return lists[start];
+    } else if (start < end){
+        const mid = parseInt((start + end)/2);
+        const left = helper(lists, start, mid);
+        const right = helper(lists, mid+1, end);
+        return merge(left, right);
+    } else {
+        return null;
+    }
+}
+
+var mergeKLists = function(lists) {
+   return helper(lists, 0, lists.length - 1);
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
 Prob: 
 
 */
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: 
+
+*/
+
+

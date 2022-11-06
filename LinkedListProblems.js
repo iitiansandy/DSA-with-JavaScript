831bc8dfc3b7a6ce3089dbafae51c7e3f4fddf4c
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-06 23:21:20 +0530

    Linked List

--- LinkedListProblems.js
+++ LinkedListProblems.js
@@ -248,17 +248,64 @@ var mergeKLists = function(lists) {
 
 
 /*
-Prob: 
+Prob: Add Two Numbers
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes 
+contains a single digit. Add the two numbers and return the sum as a linked list.
 
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example: Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
 */
+
+// Method 1
+var addTwoNumbers = function(l1, l2) {
+    var List = new ListNode(0);
+    var head = List;
+    var sum = 0;
+    var carry = 0;
+
+    while(l1 !== null || l2 !== null || sum > 0) {
+        if(l1 !== null){
+            sum = sum + l1.val;
+            l1 = l1.next;
+        }
+        if(l2 !== null){
+            sum = sum + l2.val;
+            l2 = l2.next;
+        }
+        if(sum >= 10){
+            carry = 1;
+            sum = sum - 10;
+        }
+        head.next = new ListNode(sum);
+        head = head.next;
+        sum = carry;
+        carry = 0;
+    }
+    return List.next;
+};
+
+
+// Method 2
+var addTwoNumbers = function(l1, l2) {
+    return add(l1, l2, 0);
+
+    function add(l1, l2, carry) {
+        const v1 = (l1 && l1.val) || 0;
+        const v2 = (l2 && l2.val) || 0;
+        const sum = v1 + v2 + carry;
+        const newCarry = Math.floor(sum/10);
+        const val = sum%10;
+        return (l1 || l2 || carry) ? {val, next: add(l1 && l1.next, l2 && l2.next, newCarry)}: null;
+    }
+};
 
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
-/*
-Prob: 
 
-*/
 
 

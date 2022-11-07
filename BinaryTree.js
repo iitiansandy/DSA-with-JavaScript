368df9d1b7d1d56f80b14f992f5ecf8d0bc392eb
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-07 18:43:02 +0530

    Binary Tree

--- BinaryTree.js
+++ BinaryTree.js
@@ -310,6 +310,32 @@ let grid = [
 };
 
 
-
-
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Binary Tree Right Side View
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from 
+top to bottom.
+
+Example: Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+*/
+
+var rightSideView = function(root) {
+    if(!root) return [];
+    let res = [];
+    pre(root, 0);
+    return res;
+
+    function pre(node, h) {
+        if(!node) return;
+        res[h] = node.val;
+        pre(node.left, h+1);
+        pre(node.right, h+1);        
+    }
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+

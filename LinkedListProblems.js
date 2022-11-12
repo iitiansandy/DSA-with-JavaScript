92684bd799cb833402ac53ee415c66ceb0d55da0
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-12 22:36:56 +0530

    Linked List

--- LinkedListProblems.js
+++ LinkedListProblems.js
@@ -578,8 +578,51 @@ var reverseBetween = function(head, left, right) {
 
 
 /*
-Prob: 
+Prob: Partition List
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
 
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example: Input: head = [1,4,3,2,5,2], x = 3
+Output: [1,2,2,4,3,5]
 */
+
+// Method 1
+var partition = function(head, x) {
+    let fdum = new ListNode(0), bdum = new ListNode(0), front = fdum, back = bdum, curr = head;
+
+    while (curr) {
+        if(curr.val < x) front.next = curr, front = curr;
+        else back.next = curr, back = curr;
+        curr = curr.next;
+    }
+    front.next = bdum.next, back.next = null;
+    return fdum.next;
+};
+
+
+// Method 2
+var partition = function(head, x) {
+    const dummyLess = new ListNode();
+    const dummyMore = new ListNode();
+    let node = head;
+    let less = dummyLess;
+    let more = dummyMore;
+
+    while(node) {
+        if(node.val < x) {
+            less = less.next = node;
+        } else {
+            more = more.next = node;
+        }
+        node = node.next;
+    }
+    less.next = dummyMore.next;
+    more.next = null;
+    return dummyLess.next;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 

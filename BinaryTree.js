d275e424f8e2d921a40aeecec0cbc3cae6998ee3
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-14 22:47:01 +0530

    Binary Tree

--- BinaryTree.js
+++ BinaryTree.js
@@ -339,3 +339,40 @@ var rightSideView = function(root) {
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
+
+/** Prob: Binary Tree Inorder Traversal
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example: Input: root = [1,null,2,3]
+Output: [1,3,2]
+ * 
+ */
+
+// Iterative Method
+var inorderTraversal = function (root) {
+    const stack = [];
+    const res = [];
+    while (root || stack.length) {
+        if (root) {
+            stack.push(root);
+            root = root.left;
+        } else {
+            root = stack.pop();
+            res.push(root.val);
+            root = root.right;
+        }
+    }
+    return res;
+};
+
+
+// Recursive Method
+var inorderTraversal = function (root) {
+    if (!root) return [];
+
+    return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)];
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+

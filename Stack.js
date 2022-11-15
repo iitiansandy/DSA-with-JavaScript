4522e2d6ccbd82a91bc4a981a0884e662371f03a
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-15 23:01:13 +0530

    Stack

--- Stack.js
+++ Stack.js
@@ -111,4 +111,94 @@ MyQueue.prototype.empty = function() {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Reverse String with Stack
+*/
+
+let data = [];
+let curSize = data.length;
+
+function push(val) {
+    data[curSize] = val;
+    curSize++;
+}
+
+function pop() {
+    lastRemovedItem = data[curSize - 1];
+    curSize -= 1;
+    data.length = curSize;
+    return lastRemovedItem;
+}
+
+function revStr(item){
+    for(let i=0; i<item.length; i++) {
+        push(item[i]);
+    }
+
+    for(let i=0; i<item.length; i++) {
+        item[i] = pop();
+    }
+}
+// let str = 'sandeep';
+// str = str.split("");
+// revStr(str);
+// console.log(str.join(''));
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/* 
+STACK DATA STRUCTURE
+STACK IMPLEMENTATION
+*/
+
+class Stack {
+    item = [];
+    currSize;
+    maxSize;
+    constructor(size) {
+        this.maxSize = size;
+        this.currSize = this.item.length;
+    }
+
+    push(newVal){
+        if(this.currSize >= this.maxSize) {
+            console.log("stack is full");
+        } else {
+            this.item[this.currSize] = newVal;
+            this.currSize++;
+        }
+    }
+    pop(){
+        if(this.currSize > 0) {
+            this.currSize--;
+            this.item.length = this.currSize;
+        } else {
+            console.log("stack is already empty");
+        }
+    }
+    display(){
+        console.log(this.item);
+    }
+}
+
+let stack = new Stack(5);
+stack.push(20);
+stack.push(22);
+stack.push(26);
+stack.push(28);
+stack.push(33);
+stack.display();
+
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.display();
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
 

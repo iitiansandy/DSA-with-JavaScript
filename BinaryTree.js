e4fa1504584567a6b4d1b82bf6811fbd41006d36
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-21 22:32:09 +0530

    Binary Tree

--- BinaryTree.js
+++ BinaryTree.js
@@ -854,3 +854,60 @@ var sortedArrayToBST = function(nums, start=0, end=nums.length-1) {
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
+
+/*
+Prob: Populating Next Right Pointers in Each Node
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the 
+following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Example: Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right
+node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of
+each level.
+*/
+
+// BFS Method
+var connect = function(root) {
+    if(root == null) return root;
+    let queue = [root];
+    while (queue.length != 0) {
+        let next = [];
+        while (queue.length != 0) {
+            let node = queue.shift();
+            node.next = queue[0] || null;
+            if(node.left != null) {
+                next.push(node.left);
+                next.push(node.right);
+            }
+        }
+        queue = next;
+    }
+    return root;
+};
+
+
+// DFS Method
+var connect = function(root) {
+    if (!root || root.left == null) return root;
+    root.left.next = root.right;
+    root.right.next = root.next ? root.next.left : null;
+    connect(root.left);
+    connect(root.right);
+    return root;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+

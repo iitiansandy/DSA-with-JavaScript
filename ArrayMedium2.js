36e51ac3c5175a4e276c04ca5c147d7e1679136d
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-24 13:15:39 +0530

    Leetcode Array Medium

--- ArrayMedium2.js
+++ ArrayMedium2.js
@@ -792,3 +792,80 @@ var buildTree = function(inorder, postorder) {
     return null;
 };
 
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Palindrome Partitioning
+Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning 
+of s.
+
+Example:
+
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+*/
+
+// Method 1
+const isPelindrome = str => (str === str.split('').reverse().join(''));
+
+var dfs = function(s, partition, result) {
+    if (s.length == 0) {
+        result.push([...partition]);
+        return;
+    }
+
+    for(let i=1; i<=s.length; i++) {
+        let prefix = s.substring(0, i);
+        let postfix = s.substring(i);
+
+        if(isPelindrome(prefix)) {
+            partition.push(prefix);
+            dfs(postfix, partition, result);
+            partition.pop();
+        }
+    }
+    return;
+}
+
+var partition = function(s) {
+    let partition = [];
+    let result = [];
+    dfs(s, partition, result);
+    return result;
+};
+
+
+// Method 2
+var partition = function(s) {
+    function isPelin(str) {
+        let left = 0, right = str.length-1;
+
+        while(left < right) {
+            if (str[left] !== str[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    const result = [];
+    function permute(arr, str) {
+        if (!str.length) result.push(arr);
+        for (let i=1; i<=str.length; i++){
+            const subStr = str.slice(0,i);
+            if(isPelin(subStr)) {
+                permute([...arr, subStr], str.slice(i));
+            }
+        }
+    }
+    permute([], s);
+    return result;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+
+

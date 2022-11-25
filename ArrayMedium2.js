0061d651d691157076f5799e68717e7a99007d32
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-25 12:30:51 +0530

    Leetcode Array Medium

--- ArrayMedium2.js
+++ ArrayMedium2.js
@@ -867,5 +867,54 @@ var partition = function(s) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Maximum Product Subarray
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Example:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+*/
+
+// Method 1
+var maxProduct = function(nums) {
+    let prevMax = nums[0];
+    let prevMin = nums[0];
+    let result = nums[0];
+
+    for (let i=1; i<nums.length; i++) {
+        curMax = Math.max(nums[i] * prevMax, nums[i], nums[i] * prevMin);
+        curMin = Math.min(nums[i] * prevMin, nums[i], nums[i] * prevMax);
+        prevMax = curMax;
+        prevMin = curMin;
+        result = Math.max(curMax, result);
+    }
+    return result;
+};
+
+
+// Method 2
+var maxProduct = function(nums) {
+    var res = Math.max(...nums);
+    curMin = 1;
+    curMax = 1;
+    prevMax = 1;
+
+    for (const num of nums) {
+        prevMax = curMax * num;
+        curMax = Math.max(prevMax, curMin*num, num);
+        curMin = Math.min(prevMax, curMin*num, num);
+        res = Math.max(res, curMax);
+    }
+    return res;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
 
 

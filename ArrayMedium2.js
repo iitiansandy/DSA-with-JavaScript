bcc91d86a16f3caad3769ed00825a425900de51c
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-11-27 18:27:00 +0530

    Leetcode Array Medium

--- ArrayMedium2.js
+++ ArrayMedium2.js
@@ -974,5 +974,51 @@ var isHappy = function(n) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Reverse Bits
+Reverse bits of a given 32 bits unsigned integer.
+Example 1:
+
+Input: n = 00000010100101000001111010011100
+Output:    964176192 (00111001011110000010100101000000)
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 
+which its binary representation is 00111001011110000010100101000000.
+*/
+
+// Method 1
+var reverseBits = function(n) {
+    var result = 0;
+    var count = 32;
+
+    while(count--) {
+        result *= 2;
+        result += n & 1;
+        n = n >> 1;
+    }
+    return result;
+};
+
+
+// Method 2
+var reverseBits = function(n) {
+    return Number.parseInt(n.toString(2).split("").reverse().join("").padEnd(32, "0"), 2);
+};
+
+
+// Method 3
+var reverseBits = function(n) {
+    let res = 0, pow = 31;
+    while (n > 0) {
+        let rightMost = n & 1;
+        res = res + (rightMost << pow);
+        pow--;
+        n = n >>> 1;
+    }
+    return res >>> 0;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
 
 

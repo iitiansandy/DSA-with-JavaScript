15a93f4cc8a01509237699f3858071586dfc7e89
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-12-14 18:03:06 +0530

    Array

--- ArrayPart4.js
+++ ArrayPart4.js
@@ -1054,3 +1054,39 @@ var maxProfit = function(prices) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: N-Queens
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, 
+respectively.
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+*/
+
+var solveNQueens = function(n) {
+    const res = [];
+    backtrack(res, n);
+    return res;
+};
+
+function backtrack(res, n, board = [], r=0) {
+    if(r === n) {
+        res.push(board.map(c => '.'.repeat(c) + 'Q' + '.'.repeat(n-c-1)));
+        return;
+    }
+    for (let c=0; c<n; c++) {
+        if(!board.some((bc, br) => bc === c || bc === c+r-br || bc === c-r+br)) {
+            board.push(c);
+            backtrack(res, n, board, r+1);
+            board.pop();
+        }
+    }
+}
+
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+

a30a9e50e6eb4a94e7b41068dc66a84a3929e232
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-12-15 17:35:33 +0530

    Dynamic Programming

--- Dynamic Programming.js
+++ Dynamic Programming.js
@@ -159,5 +159,67 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Wildcard Matching
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Example: Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+*/
+
+// Levenshtein Distance Algorithm (Edit Distance)
+var isMatch = function(s, p) {
+    let dp = Array(s.length+1).fill(null).map(() => Array(p.length+1).fill(false));
+    dp[0][0] = true;
+
+    for (let i=1; i<=s.length; i++) {
+        dp[i][0] = false;
+    }
+
+    for (let i=1; i<=p.length; i++) {
+        dp[0][i] = dp[0][i-1] && p[i-1] == "*";
+    }
+
+    for (let i=1; i<=s.length; i++) {
+        for (let j=1; j<=p.length; j++) {
+            if (p[j-1] == '*') {
+                dp[i][j] = dp[i-1][j] || dp[i][j-1];
+            } else if (s[i-1] == p[j-1] || p[j-1] == '?') {
+                dp[i][j] = dp[i-1][j-1];
+            }
+        }
+    }
+    return dp[s.length][p.length];
+};
+
+
+// Recursive Method
+var isMatch = function(s, p) {
+    let dp = [];
+    var recur = function(s, p) {
+        if (!dp[s]) dp[s] = {};
+        if (dp[s][p] != null) return dp[s][p];
+
+        if (s == p || (p.includes('*') && !p.replace(/\*/g, ''))) {
+            dp[s][p] = true;
+        } else if (s.length == 0 || p.length == 0) {
+            dp[s][p] = false;
+        } else if (p[0] == '?' || p[0] == s[0]) {
+            dp[s][p] = recur(s.slice(1), p.slice(1));
+        } else if (p[0] == '*') {
+            dp[s][p] = recur(s.slice(1), p) || recur(s, p.slice(1));
+        } else {
+            dp[s][p] = false;
+        }
+        return dp[s][p];
+    }
+    return recur(s, p);
+};
+
+
 
 

e089740ea90bd6416fd9a421e020e7c74b73a608
iitiansandy <sandeep.sid.kumar10@gmail.com>
2022-12-24 19:22:27 +0530

    Interview Questions

--- Interview Questions 4.js
+++ Interview Questions 4.js
@@ -254,3 +254,93 @@ var minDepth = function(root) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+
+/*
+Prob: Path Sum
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values 
+along the path equals targetSum. A leaf is a node with no children.
+Example: Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+*/
+
+// Method 1
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false;
+
+    if(!root.left && !root.right) {
+        return targetSum === root.val;
+    } else {
+        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
+    }
+};
+
+
+// Method 2
+var hasPathSum = function(root, targetSum) {
+    return dfs(root, 0, targetSum);
+};
+
+var dfs = function(curr, currSum, targetSum) {
+    if(!curr) return false;
+    currSum += curr.val;
+    if (curr.left === null && curr.right === null) {
+        return currSum === targetSum;
+    }
+    return dfs(curr.left, currSum, targetSum) || dfs(curr.right, currSum, targetSum);
+}
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Path Sum II
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals 
+targetSum. Each path should be returned as a list of the node values, not node references.
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+Example: Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+*/
+
+// Method 1
+var pathSum = function(root, targetSum, res = [], path = []) {
+    if (root) {
+        path.push(root.val);
+        if (!root.left && !root.right && targetSum-root.val === 0) res.push([...path]);
+        pathSum(root.left, targetSum-root.val, res, path);
+        pathSum(root.right, targetSum-root.val, res, path);
+        path.pop();
+    }
+    return res;
+};
+
+
+// Method 2
+var pathSum = function(root, targetSum) {
+    function dfs(node, curPath, curTarget) {
+        if (node == null) return;
+        curPath.push(node.val);
+        curTarget -= node.val;
+
+        if (node.left == null && node.right == null && curTarget == 0) {
+            answer.push(curPath.slice());
+            curPath.pop();
+            return;
+        }
+        dfs(node.left, curPath, curTarget);
+        dfs(node.right, curPath, curTarget);
+        curPath.pop();
+        return;
+    }
+    answer = [];
+    dfs(root, [], targetSum);
+    return answer;
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+

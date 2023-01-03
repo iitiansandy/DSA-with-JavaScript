dce5ac68296547a9bb53e816554141601f55e75e
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-01-03 22:54:09 +0530

    Interview Questions

--- Interview Questions 4.js
+++ Interview Questions 4.js
@@ -1311,3 +1311,47 @@ var maximalRectangle = function(matrix) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+
+/*
+Prob: Scramble String
+We can scramble a string s to get a string t using the following algorithm:
+If the length of the string is 1, stop.
+If the length of the string is > 1, do the following:
+Split the string into two non-empty substrings at a random index, i.e., if the string is s, divide it to x and y where s = x + y.
+Randomly decide to swap the two substrings or to keep them in the same order. i.e., after this step, s may become s = x + y or s = y + x.
+Apply step 1 recursively on each of the two substrings x and y.
+Given two strings s1 and s2 of the same length, return true if s2 is a scrambled string of s1, otherwise, return false.
+
+Example:
+Input: s1 = "great", s2 = "rgeat"
+Output: true
+*/
+
+var isScramble = function(s1, s2) {
+    return helper(s1, s2, {})
+};
+
+var helper = function(s1, s2, dp) {
+    if (dp[s1 + s2] != undefined || dp[s2 + s1] != undefined) {
+        return dp[s1 + s2]
+    } else if (s1 == s2) {
+        return true
+    } else if (s1.length != s2.length) {
+        return false
+    } else if (s1.length <= 1) {
+        return s1 == s2
+    }
+    for (let i = 1; i < s1.length; i++) {
+        if ((helper(s1.slice(0, i), s2.slice(0, i), dp) && helper(s1.slice(i), s2.slice(i), dp)) ||
+            (helper(s1.slice(0, i), s2.slice(s1.length - i), dp) && helper(s1.slice(i), s2.slice(0, s1.length - i), dp))) {
+            dp[s1 + s2] = true
+            return true
+        }
+    }
+    dp[s1 + s2] = false
+    return false
+};
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+

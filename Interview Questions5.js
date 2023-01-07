774045ac1bf0325af0533f938b8a5fa10c83496d
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-01-07 23:02:26 +0530

    Interview Questions

--- Interview Questions5.js
+++ Interview Questions5.js
@@ -127,3 +127,143 @@ var numDistinct = function(s, t) {
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Longest Repeating Character Replacement
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation 
+at most k times.
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+*/
+
+var characterReplacement = function(s, k) {
+    let left = 0, right = 0;
+    let maxCharCount = 0;
+    const visited = {};
+    
+    while (right < s.length) {
+        const char = s[right];
+        visited[char] = ( visited[char] || 0 ) + 1;
+        if(visited[char] > maxCharCount) maxCharCount = visited[char];
+
+        if(right-left+1-maxCharCount > k) {
+            visited[s[left]]--;
+            left++;
+        }
+        right++;
+    }
+    return right-left;
+};
+
+// let s = "ABAB", k=2;
+// console.log(characterReplacement(s,k));
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Sorting an array
+Input: [3, 6, 1, 7, 2, 9, 4, 2, 8, 5]
+Output: [1, 2, 2, 3, 4, 5, 6, 7, 8, 9] ascending order
+Output: [9, 8, 7, 6, 5, 4, 3, 2, 2, 1] descending order
+*/
+
+function sort(arr) {
+    for (let i=0; i<arr.length-1; i++) 
+    {
+        for (let j=0; j<arr.length-i-1; j++) 
+        {
+            if(arr[j] < arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// let arr = [3, 6, 1, 7, 2, 9, 4, 2, 8, 5];
+// console.log(sort(arr));
+
+
+function sortAscending(arr) {
+    for (let i=0; i<arr.length; i++) {
+        for (let j=i+1; j<arr.length; j++) {
+            if(arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+// let arr = [3, 6, 1, 7, 2, 9, 4, 2, 8, 5];
+// console.log(sortAscending(arr));
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Write a program to remove duplicate item from array
+*/
+
+function removeDuplicates(arr) {
+    let b = [];
+    for (let i=0; i<arr.length; i++) {
+        if (b.indexOf(arr[i]) == -1) {
+            b.push(arr[i]);
+        }
+    }
+    return b;
+}
+// let arr = [1,2,3,4,1,2];
+// console.log(removeDuplicates(arr));
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*
+Prob: Write a function in Javascript to traverse 2D square matrix in a zig-zag manner.
+You need to write a function which traverses 2D matrix as shown in the image below. It would visit every cell in the order of specified 
+number, and print it’s co-ordinates.
+
+Example:
+Input: traverseMatrix(1)
+Output-> (0,0)
+Input: traverseMatrix(2)
+Output-> (1,0), (0,0), (1,1), (0,1)
+Input: traverseMatrix(3)
+Output-> (2,0),(1,0),(2,1),(2,2),(1,1),(0,0),(0,1),(1,2),(0,2)
+Input: traverseMatrix(4)
+Output-> (3,0),(2,0),(3,1),(3,2),(2,1),(1,0),(0,0),(1,1),(2,2),(3,3),(2,3),(1,2),(0,1),(0,2),(1,3),(0,3)
+Input: traverseMatrix(5)
+Output-> (4,0),(3,0),(4,1),(4,2),(3,1),(2,0),(1,0),(2,1),(3,2),(4,3),(4,4),(3,3),(2,2),(1,1),(0,0),(0,1),(1,2),(2,3),(3,4),(2,4),(1,3),
+(0,2),(0,3),(1,4),(0,4)
+
+*/
+
+function traverseMatrix(n) {
+    if (n === 0) return [];
+
+    let arr = new Array(n * n).fill(null).map(() => []);
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if ((i + j) % 2 === 0) arr[i + j].unshift([i, j]);
+            else arr[i + j].push([i, j]);
+        }
+    }
+    return arr.reverse().flat();
+}
+// console.log(traverseMatrix(3));
+
+

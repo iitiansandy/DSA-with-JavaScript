1110d6ec9550d82145f88f4dcd4de03bfd25713f
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-01-09 18:07:09 +0530

    Interview Questions

--- Interview Questions5.js
+++ Interview Questions5.js
@@ -303,3 +303,42 @@ var minimumTotal = function(triangle) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Best Time to Buy and Sell Stock III
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+Find the maximum profit you can achieve. You may complete at most two transactions.
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Example:
+Input: prices = [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+*/
+
+var maxProfit = function(prices) {
+    if (prices.length === 0) return 0;
+
+    let dp = new Array(prices.length).fill(0);
+    let min = prices[0];
+    let max = 0;
+    for (let i=1; i<prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        max = Math.max(max, prices[i] - min);
+        dp[i] = max;
+    }
+
+    min = prices[0];
+    max = 0;
+    for (let i=1; i<prices.length; i++) {
+        min = Math.min(min, prices[i]-dp[i]);
+        max = Math.max(max, prices[i]-min);
+        dp[i] = max;
+    }
+    return dp.pop();
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+

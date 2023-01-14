71c6ea811042abd87577e20a9899fce0ab33b629
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-01-14 18:14:56 +0530

    Interview Questions

--- Interview Questions5.js
+++ Interview Questions5.js
@@ -563,3 +563,40 @@ function dfs(board, i, j) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Word Break II
+Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. 
+Return all such possible sentences in any order.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example: Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+Output: ["cats and dog","cat sand dog"]
+*/
+
+var wordBreak = function(s, wordDict) {
+    const memo = new Map();
+
+    function run(str) {
+        if (memo.has(str)) return memo.get(str);
+        if (!str.length) return [];
+        const result = [];
+
+        for (let word of wordDict) {
+            if (str.startsWith(word)) {
+                const next = str.slice(word.length);
+                const paths = run(next);
+                if (!paths.length && !next.length) result.push(word);
+                result.push(...paths.map(rest => word + ' ' + rest));
+            }
+        }
+        memo.set(str, result);
+        return result;
+    }
+    return run(s);
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+

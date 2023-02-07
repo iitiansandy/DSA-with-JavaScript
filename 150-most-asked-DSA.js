4865de0fc59ad44a10d110d702f26d1b8667a57e
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-02-07 19:22:07 +0530

    150 DSA Questions

--- 150-most-asked-DSA.js
+++ 150-most-asked-DSA.js
@@ -572,3 +572,32 @@ var groupAnagrams = function(strs) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Reduce Array Size to The Half
+You are given an integer array arr. You can choose a set of integers and remove all the occurrences of these integers in the array.
+Return the minimum size of the set so that at least half of the integers of the array are removed.
+Example: Input: arr = [3,3,3,3,5,5,5,2,2,7]
+Output: 2
+Explanation: Choosing {3,7} will make the new array [5,5,5,2,2] which has size 5 (i.e equal to half of the size of the old array).
+Possible sets of size 2 are {3,5},{3,2},{5,2}.
+Choosing set {2,7} is not possible as it will make the new array [3,3,3,3,5,5,5] which has a size greater than half of the size of the old array.
+*/
+var minSetSize = function(arr) {
+    const freq = {};
+    let count = 0;
+    for (let i=0; i<arr.length; i++) {
+        freq[arr[i]] ? freq[arr[i]]++ : (freq[arr[i]] = 1);
+    }
+    const sortedIntFreq = Object.values(freq).sort((a,b) => b-a);
+    for (let i=0; i<sortedIntFreq.length; i++) {
+        count += sortedIntFreq[i];
+        if (count >= arr.length/2) {
+            return (i+1);
+        }
+    }
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+

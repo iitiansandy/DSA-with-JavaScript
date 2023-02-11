6ff8828145a94387b264c86e484a3185094f4668
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-02-12 01:26:34 +0530

    150 DSA Questions

--- 150-most-asked-DSA.js
+++ 150-most-asked-DSA.js
@@ -712,3 +712,32 @@ var isPalindrome = function(head) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Linked List Cycle
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, 
+pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example: Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+*/
+
+var hasCycle = function(head) {
+    const seen = new Set();
+
+    function traverse(node){
+        if(seen.has(node)) return true;
+        if(!node) return false;
+        seen.add(node);
+        return traverse(node.next);
+    }
+    return traverse(head);
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+

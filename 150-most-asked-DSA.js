1947d027798b09b1b56fda7b68f42a4b74439618
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-02-13 22:14:13 +0530

    150 DSA Questions

--- 150-most-asked-DSA.js
+++ 150-most-asked-DSA.js
@@ -761,3 +761,30 @@ var getIntersectionNode = function(headA, headB) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Remove Linked List Elements
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+Ex: Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+*/
+
+var removeElements = function(head, val) {
+    if (!head) return head;
+    while (head) {
+        if (head.val === val) {
+            head = head.next;
+        } else {
+            break;
+        }
+    }
+    let curr = head;
+    while (curr && curr.next) {
+        if (curr.next.val === val) curr.next = curr.next.next;
+        else curr = curr.next;
+    }
+    return head;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+

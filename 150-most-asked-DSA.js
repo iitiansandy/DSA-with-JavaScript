6fb05ff7636834b812f022c740f95c35dd0f0f88
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-02-18 22:38:43 +0530

    150 DSA Questions

--- 150-most-asked-DSA.js
+++ 150-most-asked-DSA.js
@@ -788,3 +788,27 @@ var removeElements = function(head, val) {
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
+
+/*
+Prob: Middle of the Linked List
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Example: Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+*/
+
+var middleNode = function(head) {
+    let fast = head, slow = head;
+    while(fast && fast.next){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+

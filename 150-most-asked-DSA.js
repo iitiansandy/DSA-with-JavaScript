359b6aa8eff648c5fc1e625091dd312d7a16d56a
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-02-19 23:19:41 +0530

    150 DSA Questions

--- 150-most-asked-DSA.js
+++ 150-most-asked-DSA.js
@@ -812,3 +812,55 @@ var middleNode = function(head) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Merge k Sorted Lists
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example: Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+*/
+
+function merge (left, right){
+    if(!left){
+        return right;
+    } else if(!right) {
+        return left;
+    } else if(left.val < right.val) {
+        left.next = merge(left.next, right);
+        return left;
+    } else {
+        right.next = merge(left, right.next);
+        return right;
+    }
+}
+
+function helper (lists, start, end) {
+    if(start === end) {
+        return lists[start];
+    } else if (start < end){
+        const mid = parseInt((start + end)/2);
+        const left = helper(lists, start, mid);
+        const right = helper(lists, mid+1, end);
+        return merge(left, right);
+    } else {
+        return null;
+    }
+}
+
+var mergeKLists = function(lists) {
+   return helper(lists, 0, lists.length - 1);
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+

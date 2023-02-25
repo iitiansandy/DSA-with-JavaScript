d4de0e86e432f468610a78d852be411a51fc3bad
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-02-25 21:07:04 +0530

    150 DSA Questions

--- 150-most-asked-DSA.js
+++ 150-most-asked-DSA.js
@@ -864,3 +864,25 @@ var mergeKLists = function(lists) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: Intersection of Two Arrays
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you 
+may return the result in any order.
+Example: Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+*/
+
+var intersection = function(nums1, nums2) {
+    const setOne = new Set(nums1);
+    const setTwo = new Set(nums2);
+    let arr = [];
+    setTwo.forEach(e => {
+        if(setOne.has(e))
+            arr.push(e); 
+    })
+    return arr;
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+

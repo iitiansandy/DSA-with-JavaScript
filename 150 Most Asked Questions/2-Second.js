e07757d26266fcb7f16888a3ff62e24e8c793ce5
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-03-19 18:28:27 +0530

    150 DSA Questions

--- 150 Most Asked Questions/2-Second.js
+++ 150 Most Asked Questions/2-Second.js
@@ -296,6 +296,38 @@ const totalFruit = array => {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 
+/*
+Prob: 35. Search Insert Position
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would 
+be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+Example : Input: nums = [1,3,5,6], target = 5
+Output: 2
+*/
+
+var searchInsert = function(nums, target) {
+    if(target < nums[0]) return 0;
+    if(target > nums[nums.length-1]) return nums.length;
+    let left = 0;
+    let right = nums.length-1;
+    
+    while(left <= right){
+        let mid = Math.floor((left + right)/2);
+        
+        if(nums[mid] === target) return mid;
+        
+        if(nums[mid]>target && nums[mid-1]<target) return mid;
+        
+        if(nums[mid] < target) left = mid+1;
+        
+        else if(nums[mid] > target) right = mid-1;
+    }
+};
+
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
 
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
